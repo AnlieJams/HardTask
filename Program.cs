b0805bd4768a19b3a49309633d4c8a1ab883e1f8
AnlieJams <98762736+AnlieJams@users.noreply.github.com>
2023-06-25 10:57:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        string[] digitPatterns =
+        {
+            "*****\n*   *\n*   *\n*   *\n*****",
+            "    *\n    *\n    *\n    *\n    *",
+            "*****\n    *\n*****\n*    \n*****",
+            "*****\n    *\n*****\n    *\n*****",
+            "*   *\n*   *\n*****\n    *\n    *",
+            "*****\n*    \n*****\n    *\n*****",
+            "*****\n*    \n*****\n*   *\n*****",
+            "*****\n    *\n    *\n    *\n    *",
+            "*****\n*   *\n*****\n*   *\n*****",
+            "*****\n*   *\n*****\n    *\n*****" 
+        };
+
+        string numberString = number.ToString();
+
+        for (int row = 0; row < 5; row++)
+        {
+            foreach (char digit in numberString)
+            {
+                int index = digit - '0';
+                string digitPattern = digitPatterns[index];
+                Console.Write(digitPattern.Split('\n')[row].Replace('*', digit));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
